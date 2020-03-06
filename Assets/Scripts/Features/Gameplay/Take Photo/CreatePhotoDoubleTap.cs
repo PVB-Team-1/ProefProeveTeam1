38cdeb1f47e49ef54d4c8e0b5190bec08856af40
Demi Joseph <demi.a.joseph@gmail.com>
2020-03-06 15:23:11 +0100

    <Feature:Take Photo> <Status: In Progress>
    Add Double tap take picture
    Add Photo generating

---
+++ Assets/Scripts/Features/Gameplay/Take Photo/CreatePhotoDoubleTap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A script to tell the game to take a picture when double tapping.
+/// </summary>
+public class CreatePhotoDoubleTap : MonoBehaviour
+{
+
+    void Update()
+    {
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.tapCount == 2)
+            {
+                //PhotoCameraApi.CreatePhoto();
+                return;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Features/Gameplay/Take Photo/GeneratePhoto.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A script to generate a photograph.
+/// </summary>
+public class GeneratePhoto : MonoBehaviour
+{
+    [SerializeField] private GameObject _picture;
+    [SerializeField] private RenderTexture _renderTexture;
+
+    void Start() {
+        //PhotoCameraApi.OnCreatePhoto += GenerateNewPhoto;
+    }
+
+    /// <summary>
+    /// Create a new photograph.
+    /// </summary>
+    void GenerateNewPhoto() {
+       Renderer rend = _picture.GetComponent<Renderer>();
+        rend.material.mainTexture = ToTexture2D(_renderTexture);
+    }
+
+    /// <summary>
+    /// Returns a Texture2D generated from RenderTexture "rTex".
+    /// </summary>
+    /// <param name="rTex"></param>
+    /// <returns></returns>
+    Texture2D ToTexture2D(RenderTexture rTex)
+    {
+        Texture2D tex = new Texture2D(rTex.width, rTex.height, TextureFormat.RGB24, false);
+        RenderTexture.active = rTex;
+        tex.ReadPixels(new Rect(0, 0, rTex.width, rTex.height), 0, 0);
+        tex.Apply();
+        RenderTexture.active = null;
+        return tex;
+    }
+}
