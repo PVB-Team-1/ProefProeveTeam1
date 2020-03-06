aae32542182dda719f039e879aa4fa54cfddf104
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2020-03-06 11:51:14 +0100

    <Feature: Camera Backend> <Status: Done> Add Photo camera api Add Simple delegate event

---
+++ Assets/Scripts/Backend/Api/PhotoCameraApi.cs
@@ -0,0 +1,44 @@
+﻿/// <summary>
+/// An Api class for everything about the Photo Camera.
+/// </summary>
+public static class PhotoCameraApi
+{
+	/// <summary>
+	/// An event for when a photo should be created.
+	/// </summary>
+	public static event SimpleEvent OnCreatePhoto;
+
+	/// <summary>
+	/// An event for when a photo should be shown.
+	/// </summary>
+	public static event SimpleEvent OnShowPhoto;
+
+	/// <summary>
+	/// An event for when a photo finished showing.
+	/// </summary>
+	public static event SimpleEvent OnShowPhotoFinished;
+
+	/// <summary>
+	/// CreatePhoto calls the OnCreatePhoto event.
+	/// </summary>
+	public static void CreatePhoto()
+	{
+		OnCreatePhoto?.Invoke();
+	}
+
+	/// <summary>
+	/// ShowPhoto calls the OnShowPhoto event.
+	/// </summary>
+	public static void ShowPhoto()
+	{
+		OnShowPhoto?.Invoke();
+	}
+
+	/// <summary>
+	/// ShowPhotoFinished calls the OnShowPhotoFinished event.
+	/// </summary>
+	public static void ShowPhotoFinished()
+	{
+		OnShowPhotoFinished?.Invoke();
+	}
+}
