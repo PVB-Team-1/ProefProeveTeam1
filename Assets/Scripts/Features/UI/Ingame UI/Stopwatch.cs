8d341a41d386036590864c7454a6a79282e45e6e
Joey Kossen <joeykossen@hotmail.com>
2020-03-06 15:12:15 +0100

    <Feature: Ingame UI> <Status: In progess> Add Pause menu script

--- Assets/Scripts/Features/UI/Ingame UI/Stopwatch.cs
+++ Assets/Scripts/Features/UI/Ingame UI/Stopwatch.cs
@@ -1,20 +1,30 @@
 ﻿using UnityEngine;
 using TMPro;
 
+/// <summary>
+/// This class controls the stopwatch in the game that keeps track of how much time it takes for the player to finish the level.
+/// </summary>
 public class Stopwatch : MonoBehaviour
 {
+    // This float keeps track of the time thats passed.
     private float _stopwatchTime = 0;
 
+    // This bool checks whether the timer has to start or not.
     private bool _startStopwatch = false;
 
+    // This is the text object on which the timer is displayed.
     [SerializeField] private TextMeshProUGUI _stopwatchTimer;
 
     private void Update()
     {
+        // Enables the timer
         if (_startStopwatch)
             CountTheTime();
     }
 
+    /// <summary>
+    /// This function counts the time that has passed.
+    /// </summary>
     private void CountTheTime()
     {
         _stopwatchTime += Time.deltaTime;
@@ -22,6 +32,9 @@ public class Stopwatch : MonoBehaviour
         DisplayTheTime();
     }
 
+    /// <summary>
+    /// This class displays the time on the text object.
+    /// </summary>
     private void DisplayTheTime()
     {
         string minutes = Mathf.Floor(_stopwatchTime / 60).ToString("00");
