d61d3dee37e7d680112077d5340c742f0ef87a1e
Joey Kossen <joeykossen@hotmail.com>
2020-03-06 13:33:24 +0100

    <Feature: Ingame UI> <Status: In progess> Add Textmeshpro package Add Sprite ui lowerbar Add New font style Add Stopwatch

---
+++ Assets/Scripts/Features/UI/Ingame UI/Stopwatch.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class Stopwatch : MonoBehaviour
+{
+    private float _stopwatchTime = 0;
+
+    private bool _startStopwatch = false;
+
+    [SerializeField] private TextMeshProUGUI _stopwatchTimer;
+
+    private void Update()
+    {
+        if (_startStopwatch)
+            CountTheTime();
+    }
+
+    private void CountTheTime()
+    {
+        _stopwatchTime += Time.deltaTime;
+
+        DisplayTheTime();
+    }
+
+    private void DisplayTheTime()
+    {
+        string minutes = Mathf.Floor(_stopwatchTime / 60).ToString("00");
+        string seconds = Mathf.Floor(_stopwatchTime % 60).ToString("00");
+
+        _stopwatchTimer.text = minutes + ":" + seconds;
+    }
+
+    public void EnableTheStopwatch(bool enable) { _startStopwatch = enable; }
+}
