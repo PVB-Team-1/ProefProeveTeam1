d0d9def591098df88418ae7aa1fa84ff72f28092
Casper Meloen <cemeloen@gmail.com>
2020-03-11 11:12:39 +0100

    Revert from ingame-ui

--- Assets/Scripts/Events/GameObjectEvent.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine; 
-
-/// <summary>
-/// A delegate with a GameObject parameter.
-/// </summary>
-/// <param name="value">A GameObject parameter.</param>
-public delegate void GameObjectEvent(GameObject value);
