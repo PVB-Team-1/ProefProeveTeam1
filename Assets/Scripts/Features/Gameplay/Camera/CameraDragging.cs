610eab94f3253219450ee85d95d9f1e11e16e974
Koen Sanderse <koensanderse2000@gmail.com>
2020-03-11 11:38:31 +0100

    <Feature: Camera Controls> <Status: Done> Fix Touch Input instead of Mous

--- Assets/Scripts/Features/Gameplay/Camera/CameraDragging.cs
+++ Assets/Scripts/Features/Gameplay/Camera/CameraDragging.cs
@@ -8,10 +8,6 @@ public class CameraDragging : MonoBehaviour
 {
     [SerializeField]
     ///<summary>
-    /// dragSpeed determines how fast the camera moves when dragging the screen.
-    ///</summary>
-    public float dragSpeed = 4;
-    ///<summary>
     /// The camera boundary on the left side.
     ///</summary>
     public float leftBoundary = -3f;
@@ -28,68 +24,99 @@ public class CameraDragging : MonoBehaviour
     ///</summary>
     public float downBoundary = -3f;
 
-    private Vector3 _oldPos;
-    private Vector3 _mouseOrigin;
+    ///<summary>
+    /// dragSpeed determines how fast the camera moves when dragging the screen.
+    ///</summary>
+    [SerializeField]
+    private float _dragSpeed = 4;
 
-    private bool FoundMultipleTouches()
-    {
-        return Input.touchCount > 1;
-    }
+    private bool _isDragging = false;
+
+    private Vector3 _oldPos;
+    private Vector3 _touchOrigin;
+
+    private Touch touch;
 
     /*
         SavePositions saves the current position of the current object into the _oldPos private variable.
-        Saves the current mouse position on the moment of time, and saves it into _mouseOrigin private variable.
+        Saves the current touch position on the moment of time, and saves it into _touchOrigin private variable.
     */
     private void SaveOriginPositions()
     {
         _oldPos = transform.position;
-        _mouseOrigin = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        _touchOrigin = Camera.main.ScreenToViewportPoint(Input.GetTouch(0).position);
     }
 
     /*
-        Moves the object according to the dragging the cursor/finger across the screen.
+        Moves the object according to the dragging the finger across the screen.
     */
     private void MoveObject()
     {
-        //Get the new position of the mouse relative to the _mouseOrigin
-        Vector3 newPos = Camera.main.ScreenToViewportPoint(Input.mousePosition) - _mouseOrigin;
+        //Get the new position of the mouse relative to the _touchOrigin
+        Vector3 newPos = Camera.main.ScreenToViewportPoint(Input.GetTouch(0).position) - _touchOrigin;
 
         //Calculate the position of the next step, for checking if the next position is over a boundary
-        float nextStepX = transform.position.x + -newPos.x * dragSpeed;
-        float nextStepY = transform.position.y + -newPos.y * dragSpeed;
+        float nextStepX = transform.position.x + -newPos.x * _dragSpeed;
+        float nextStepY = transform.position.y + -newPos.y * _dragSpeed;
 
         //Check if the nextSteps are outside the boundaries
-        if (nextStepX < rightBoundary && nextStepX > leftBoundary)
+        if
+        (
+            nextStepX < rightBoundary &&
+            nextStepX > leftBoundary
+        )
         {
             //move the object horizontally
-            transform.position = new Vector3(_oldPos.x + -newPos.x * dragSpeed, transform.position.y, transform.position.z);
+            transform.position = new Vector3
+            (
+                _oldPos.x + -newPos.x * _dragSpeed,
+                transform.position.y,
+                transform.position.z
+            );
         }
-        if (nextStepY < upBoundary && nextStepY > downBoundary)
+        if
+        (
+            nextStepY < upBoundary &&
+            nextStepY > downBoundary
+        )
         {
             //move the object vertically
-            transform.position = new Vector3(transform.position.x, _oldPos.y + -newPos.y * dragSpeed, transform.position.z);
+            transform.position = new Vector3
+            (
+                transform.position.x,
+                _oldPos.y + -newPos.y * _dragSpeed,
+                transform.position.z
+            );
         }
     }
     private void Update()
     {
         //only run when there is not more than 1 input.
-        if (!FoundMultipleTouches())
+        switch (Input.touchCount)
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-                SaveOriginPositions();
-            }
-
-            if (Input.GetMouseButton(0))
-            {
-                MoveObject();
-            }
-        } 
-        
-        if(FoundMultipleTouches()){
-            //reset the _oldPos and _mouseOrigin
-            _oldPos = transform.position;
-            _mouseOrigin = Input.mousePosition;
+            case 1:
+                if (touch.phase == TouchPhase.Began)
+                {
+                    if (!_isDragging)
+                    {
+                        SaveOriginPositions();
+                        _isDragging = true;
+                    }
+                    else
+                    {
+                        MoveObject();
+                    }
+                }
+                break;
+            case 0:
+                _isDragging = false;
+                break;
+            default:
+                //reset the _oldPos and _touchOrigin
+                _oldPos = transform.position;
+                _touchOrigin = Camera.main.ScreenToViewportPoint(Input.GetTouch(0).position);
+                _isDragging = false;
+                break;
         }
     }
 }
