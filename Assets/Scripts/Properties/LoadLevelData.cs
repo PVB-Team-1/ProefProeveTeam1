fe6d1006283305ceea6f22c72d2df62385aa11f1
Casper Meloen <cemeloen@gmail.com>
2020-03-16 16:13:16 +0100

    <Feature: Find Object Backend> <Status: Done> Add FoundObject method and event in PhotoCameraApi Add LevelData and currentFindableItems in Properties Add Item Spawner tag Add Hidden class for randomization of findable items Add LevelData class Add LoadLevelData class to load all data in

---
+++ Assets/Scripts/Properties/LoadLevelData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Sends a container of the LevelData class to the Properties class.
+/// </summary>
+public class LoadLevelData : MonoBehaviour
+{
+	/// <summary>
+	/// The container for all data in each level.
+	/// </summary>
+	[SerializeField] private LevelData[] _levelData;
+
+	private void Awake()
+	{
+		Properties.levelData = _levelData;
+	}
+}
