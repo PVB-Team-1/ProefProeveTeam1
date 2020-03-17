359ec53bec4aa8a41aeeee410096a8330012b9d1
projojoboy <joeykossen1@gmail.com>
2020-03-17 13:47:19 +0100

    <Feature: Objects To FInd UI> <Status: Done> Add Fade when found object

--- Assets/Scripts/Features/UI/Ingame UI/ObjectsToFindBar.cs
+++ Assets/Scripts/Features/UI/Ingame UI/ObjectsToFindBar.cs
@@ -3,11 +3,18 @@ using UnityEngine.UI;
 
 public class ObjectsToFindBar : MonoBehaviour
 {
-    private GameObject _ItemContainer = null;
+    private GameObject _itemContainer = null;
+
+    private Image _foundObject = null;
+    private Image[] _spawnedImages = null;
+
+    private bool _lerpColor = false;
 
     private void Awake()
     {
-        _ItemContainer = FindObjectOfType<HorizontalLayoutGroup>().gameObject;
+        _itemContainer = FindObjectOfType<HorizontalLayoutGroup>().gameObject;
+
+        PhotoCameraApi.OnFoundObject += ObjectFound;
     }
 
     private void Start()
@@ -16,15 +23,39 @@ public class ObjectsToFindBar : MonoBehaviour
         LevelApi.StartLevel(1);
     }
 
+    private void Update()
+    {
+        if(_lerpColor)
+            LerpImageColor(_foundObject, Color.black, Color.white, 3);
+    }
+
+    private void LerpImageColor(Image image, Color from, Color to, float duration)
+    {
+        if (image.color != Color.white)
+            image.color = Color.Lerp(from, to, duration * Time.deltaTime);
+        else
+            _lerpColor = false;
+    }
+
+    private void ObjectFound(int foundObject)
+    {
+        _foundObject = _spawnedImages[foundObject];
+        _lerpColor = true;
+    }
+
     private void SpawnFindableObjectUI(int level)
     {
         int length = Properties.levelData[level - 1].itemIcons.Length;
+        _spawnedImages = new Image[length];
 
         for (int i = 0; i < length; i++)
         {
-            Image spawnedUI = Instantiate(new GameObject("UI_Image - Item_Icon"), _ItemContainer.transform.position, Quaternion.identity, _ItemContainer.transform).AddComponent<Image>();
+            Image spawnedUI = Instantiate(new GameObject("UI_Image - Item_Icon"), _itemContainer.transform.position, Quaternion.identity, _itemContainer.transform).AddComponent<Image>();
             spawnedUI.sprite = Properties.levelData[level - 1].itemIcons[i];
+            spawnedUI.color = Color.black;
             spawnedUI.preserveAspect = true;
+
+            _spawnedImages[i] = spawnedUI;
         }
     }
 }
