d2bf5d73f1295d3def3edb4e1b2dafd98864b389
projojoboy <joeykossen1@gmail.com>
2020-03-17 13:23:23 +0100

    <Feature: Objects To FInd UI> <Status: Done> Add Objects to find front end

---
+++ Assets/Scripts/Features/UI/Ingame UI/ObjectsToFindBar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectsToFindBar : MonoBehaviour
+{
+    private GameObject _ItemContainer = null;
+
+    private void Awake()
+    {
+        _ItemContainer = FindObjectOfType<HorizontalLayoutGroup>().gameObject;
+    }
+
+    private void Start()
+    {
+        LevelApi.OnLevelStart += SpawnFindableObjectUI;
+    }
+
+    private void SpawnFindableObjectUI(int i)
+    {
+        GameObject spawnedUI = Instantiate(new GameObject("UI_Image - Item_Icon"), _ItemContainer.transform.position, Quaternion.identity);
+        spawnedUI.AddComponent<Image>().sprite = ;
+    }
+}
