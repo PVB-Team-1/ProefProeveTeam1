75dca6b56514adbd5ebef061cd12f8825af91296
Demi Joseph <demi.a.joseph@gmail.com>
2020-03-17 16:19:56 +0100

    <Feature: Show Photo> <Status: Done> Add PC controls for taking pictures Add Disable taking pictures while photo is visible Add Show and hide taken picture Remove Reference to external photo object Add UI Photo prefab to appear and disappear Add Update Feature Take Picture scene with all current functionality of taking and showing photos

--- Assets/Scripts/Features/Gameplay/Take Photo/CreatePhotoDoubleTap.cs
+++ Assets/Scripts/Features/Gameplay/Take Photo/CreatePhotoDoubleTap.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 /// <summary>
 /// A script to tell the game to take a picture when double tapping.
@@ -6,8 +7,44 @@
 public class CreatePhotoDoubleTap : MonoBehaviour
 {
 
+    private bool _doubleTapEnabled = true;
+
+    private int _clickCount = 0;
+
+    private float _maxClickTime = 0.35f;
+
+    private void Start()
+    {
+        // Disable double tapping while a photo is shown.
+        PhotoCameraApi.OnShowPhotoFinished += EnableDoubleTap;
+        PhotoCameraApi.OnCreatePhoto += DisableDoubleTap;
+    }
+
     private void Update()
     {
+        if (!_doubleTapEnabled)
+            return;
+
+#if UNITY_EDITOR || UNITY_STANDALONE_WIN
+
+        // When clicking, start a timer to check for a 2nd click.
+        if (Input.GetMouseButtonDown(0))
+        {
+            _clickCount++;
+            StartCoroutine(DoubleClickTimer());
+        }
+
+        if (_clickCount == 2)
+        {
+            _clickCount = 0;
+
+            PhotoCameraApi.CreatePhoto();
+        }
+
+#endif
+
+#if UNITY_ANDROID
+
         foreach (Touch touch in Input.touches)
         {
             if (touch.tapCount == 2)
@@ -17,5 +54,34 @@ public class CreatePhotoDoubleTap : MonoBehaviour
                 return;
             }
         }
+
+#endif
+
     }
+
+    /*
+        Resets click count to 0 after the maximum time to doubleclick has passed.
+    */
+    private IEnumerator DoubleClickTimer()
+    {
+        yield return new WaitForSeconds(_maxClickTime);
+        _clickCount = 0;
+    }
+
+    private void EnableDoubleTap()
+    {
+        _doubleTapEnabled = true;
+    }
+
+    private void DisableDoubleTap()
+    {
+        _doubleTapEnabled = false;
+    }
+
+    private void OnDestroy()
+    {
+        PhotoCameraApi.OnShowPhotoFinished -= EnableDoubleTap;
+        PhotoCameraApi.OnCreatePhoto -= DisableDoubleTap;
+    }
+
 }
